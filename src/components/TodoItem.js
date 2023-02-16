6557353c9b9abf5737c1322d8dbe6b0d65d97b50
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-16 09:07:26 +0000

    static loading of images and rendering in TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Image from 'next/image';
+
+import IconCheck from '../../public/images/icon-check.svg';
+import IconCross from '../../public/images/icon-cross.svg';
 
 const TodoItem = ({ children }) => {
     return (
-        <div>
+        <div className='flex items-center'>
+            <Image src={IconCheck} width={30} height={30} />
             <p>{children}</p>
+            <Image src={IconCross} />
         </div>
     );
 };
