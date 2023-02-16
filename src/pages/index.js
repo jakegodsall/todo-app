15ee4f58564e1e34905b366d616d42e2cc0877c6
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-16 11:17:33 +0000

    added id to dummy data

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,32 @@ import TodoList from '@/components/TodoList';
 
 const DUMMY_DATA = [
     {
+        id: 1,
         content: 'Complete online JavaScript course',
         complete: true,
     },
     {
+        id: 2,
         content: 'Jog around the park 3x',
         complete: false,
     },
     {
+        id: 3,
         content: '10 minutes meditation',
         complete: false,
     },
     {
+        id: 4,
         content: 'Read for 1 hour',
         complete: false,
     },
     {
+        id: 5,
         content: 'Pick up groceries',
         complete: false,
     },
     {
+        id: 6
         content: 'Complete Todo App on Frontend Mentor',
         complete: false,
     },
